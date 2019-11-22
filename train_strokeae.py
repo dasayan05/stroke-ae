e1e8a51f60dd50593491167a8918bbe0e976bd09
Das, Ayan (PG/R - Elec Electronic Eng) <a.das@surrey.ac.uk>
2019-11-22 20:15:04 +0000

    calculate test loss

--- train_strokeae.py
+++ train_strokeae.py
@@ -8,10 +8,12 @@ from quickdraw.quickdraw import QuickDraw
 from strokeae import RNNStrokeAE, StrokeMSELoss
 
 def main( args ):
-    qds = QuickDraw(args.root, max_samples=5000, mode=QuickDraw.STROKE, #start_from_zero=True,
-        categories=['airplane', 'bus'], verbose=True,
+    qds = QuickDraw(args.root, max_samples=50000, mode=QuickDraw.STROKE, #start_from_zero=True,
+        categories=['airplane', 'bus', 'face'], verbose=True,
         seperate_p_tensor=True, shifted_seq_as_supevision=True)
-    qdl = qds.get_dataloader(args.batch_size)
+    qds_train, qds_test = qds.split(0.8)
+    qdl_train = qds_train.get_dataloader(args.batch_size)
+    qdl_test = qds_test.get_dataloader(args.batch_size)
 
     model = RNNStrokeAE(2, args.hidden, args.layers, 2, bidirectional=args.bidirec)
     model = model.float()
@@ -27,7 +29,27 @@ def main( args ):
 
     count = 0
     for e in range(args.epochs):
-        for i, ((X, _), (Y, P), _) in enumerate(qdl):
+
+        model.eval()
+        avg_loss, c = 0., 0
+        for i, ((X, _), (Y, P), _) in enumerate(qdl_test):
+            (Y, L), (P, _) = pad_packed_sequence(Y, batch_first=True), pad_packed_sequence(P, batch_first=True)
+
+            h_initial = torch.zeros(args.layers * (2 if args.bidirec else 1), X.batch_sizes.max(), args.hidden, dtype=torch.float32)
+            if torch.cuda.is_available():
+                X, Y, P = X.cuda(), Y.cuda(), P.cuda()
+                h_initial = h_initial.cuda()
+            
+            out, p = model(X, h_initial)
+
+            loss = strokemse(out, p, Y, P, L)
+            avg_loss = ((avg_loss * c) + loss.item()) / (c + 1)
+            c += 1
+        print(f'[Testing: -/{e}/{args.epochs}] -> Loss: {avg_loss}')
+        writer.add_scalar('test_loss', avg_loss, global_step=count)
+
+        model.train()
+        for i, ((X, _), (Y, P), _) in enumerate(qdl_train):
             (Y, L), (P, _) = pad_packed_sequence(Y, batch_first=True), pad_packed_sequence(P, batch_first=True)
 
             h_initial = torch.zeros(args.layers * (2 if args.bidirec else 1), X.batch_sizes.max(), args.hidden, dtype=torch.float32)
@@ -49,13 +71,15 @@ def main( args ):
                     saved = False
 
                 saved_string = ' (saved)' if saved else ''
-                print(f'[{i}/{e}/{args.epochs}] -> Loss: {loss}{saved_string}')
+                print(f'[Training: {i}/{e}/{args.epochs}] -> Loss: {loss}{saved_string}')
                 
-                writer.add_scalar('loss', loss.item(), global_step=count)
+                writer.add_scalar('train_loss', loss.item(), global_step=count)
 
             optim.zero_grad()
             loss.backward()
             optim.step()
+
+
 
 if __name__ == '__main__':
     import argparse
